9ae26ec4bee74ec145a2c4c8f25346ee634aac66
Pritam Jana <pritam.jana@sbnri.com>
2023-06-03 11:31:59 +0530

    remove static files

--- stickynotes/urls.py
+++ stickynotes/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from . import settings, views
+from . import views
 
 urlpatterns = [
     path('notes/', include('notes.urls')),
     path('admin/', admin.site.urls),
     path('', views.index)
 ]
-urlpatterns += staticfiles_urlpatterns()
